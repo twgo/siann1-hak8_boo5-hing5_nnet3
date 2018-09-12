af96acb4553d11571c11a21ec1bc5739254226d3
sih4sing5hong5 <ihcaoe@gmail.com>
2018-09-12 11:18:57 +0800

    dnn有訓練了

--- Dockerfile
+++ Dockerfile
@@ -30,36 +30,44 @@ ENV KALDI_S5C /usr/local/kaldi/egs/formosa/s5
 
 RUN mkdir -p $KALDI_S5C
 WORKDIR $KALDI_S5C
-COPY --from=dockerhub.iis.sinica.edu.tw/siann1-hak8_boo5-hing5:171 /usr/local/pian7sik4_gi2liau7/ /usr/local/pian7sik4_gi2liau7/
+COPY --from=dockerhub.iis.sinica.edu.tw/siann1-hak8_boo5-hing5:195 /usr/local/pian7sik4_gi2liau7/ /usr/local/pian7sik4_gi2liau7/
 ENV SIANN_KALDI_S5C /usr/local/kaldi/egs/taiwanese/s5c
-COPY --from=dockerhub.iis.sinica.edu.tw/siann1-hak8_boo5-hing5:171 $SIANN_KALDI_S5C/data $KALDI_S5C/data
-RUN mkdir -p $KALDI_S5C/exp
-COPY --from=dockerhub.iis.sinica.edu.tw/siann1-hak8_boo5-hing5:171 $SIANN_KALDI_S5C/exp/tri4 $KALDI_S5C/exp/tri4
-#COPY --from=dockerhub.iis.sinica.edu.tw/siann1-hak8_boo5-hing5:171 $SIANN_KALDI_S5C/local $KALDI_S5C/local
-COPY --from=dockerhub.iis.sinica.edu.tw/siann1-hak8_boo5-hing5:171 $SIANN_KALDI_S5C/cmd.sh $KALDI_S5C
-#COPY --from=dockerhub.iis.sinica.edu.tw/siann1-hak8_boo5-hing5:171 $SIANN_KALDI_S5C/path.sh $KALDI_S5C
-RUN ln -s ../../wsj/s5/steps steps
-RUN ln -s ../../wsj/s5/utils utils
+COPY --from=dockerhub.iis.sinica.edu.tw/siann1-hak8_boo5-hing5:195 $SIANN_KALDI_S5C/data $KALDI_S5C/data
+COPY --from=dockerhub.iis.sinica.edu.tw/siann1-hak8_boo5-hing5:195 $SIANN_KALDI_S5C/cmd.sh $KALDI_S5C
+#RUN ln -s ../../wsj/s5/steps steps
+#RUN ln -s ../../wsj/s5/utils utils
 RUN apt-get install -y sox
 RUN apt-get install -y vim
 
 RUN mkdir -p $SIANN_KALDI_S5C/
 RUN ln -s $KALDI_S5C/data $SIANN_KALDI_S5C/data
-COPY run.sh .
-RUN bash -x run.sh --stage 5
 
 ENV LANG en_US.UTF-8
-#ENV LC_ALL en_US.UTF-8
 
 RUN mkdir -p /usr/local/gi2_liau7_khoo3
 RUN ln -s /usr/local/pian7sik4_gi2liau7/twisas/音檔 /usr/local/gi2_liau7_khoo3
-RUN sed -i 's/ALen\&/ALen\\\&/g' data/train/wav.scp
+#RUN sed -i 's/-G \/usr/-G \"\/usr/g' data/train/wav.scp
+#RUN sed -i 's/wav -b/wav\" -b/g' data/train/wav.scp
+#RUN sed -i 's/\&/_/g' data/train/wav.scp
+#RUN ln -s "/usr/local/pian7sik4_gi2liau7/twisas/音檔/ALen\&Tea" /usr/local/pian7sik4_gi2liau7/twisas/音檔/ALen_Tea
 
 RUN ln -s lang_train data/lang
 
-RUN sed 's/local\/nnet3\/run_ivector_common.sh/bash -x local\/nnet3\/run_ivector_common.sh/g' -i local/chain/tuning/run_tdnn_1a.sh
-RUN bash -x local/nnet3/run_ivector_common.sh
+COPY run.sh .
+RUN bash -x run.sh --num_jobs 4
+
+# RUN sed 's/local\/nnet3\/run_ivector_common.sh/bash -x local\/nnet3\/run_ivector_common.sh/g' -i local/chain/tuning/run_tdnn_1a.sh
+RUN bash -x local/nnet3/run_ivector_common.sh --test_sets train_dev
 
 RUN ln -s train data/train_sp
+RUN sed 's/-le/-eq/g' local/chain/run_tdnn.sh -i
 RUN bash -x local/chain/run_tdnn.sh --stage 7
+RUN bash -x local/chain/run_tdnn.sh --stage 8
+RUN bash -x local/chain/run_tdnn.sh --stage 9
+RUN bash -x local/chain/run_tdnn.sh --stage 10
+CMD bash -x local/chain/run_tdnn.sh --stage 11
+
+#RUN sed 's/in test/in train_dev/g' local/chain/run_tdnn.sh -i
+#RUN bash -x local/chain/run_tdnn.sh --stage 12
+#RUN bash -x local/chain/run_tdnn.sh --stage 13
 
--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@
 # 1. Formosa Speech in the Wild (FSW) project (https://sites.google.com/speech.ntut.edu.tw/fsw/home/corpus)
 # 2. Formosa Speech Recognition Challenge (FSW) 2018 (https://sites.google.com/speech.ntut.edu.tw/fsw/home/challenge)
 stage=-2
-num_jobs=4
+num_jobs=20
 
 # shell options
 set -e -o pipefail
@@ -15,31 +15,132 @@ set -e -o pipefail
 . ./cmd.sh
 . ./utils/parse_options.sh
 
-# tri5
-if [ $stage -eq 5 ]; then
-
-  echo "$0: train tri5 model"
-  # Building a larger SAT system.
-  ln -s ../exp/tri4 exp/tri5a
-
-  # align tri5a
-  steps/align_fmllr.sh --cmd "$train_cmd" --nj $num_jobs \
-    data/train data/lang_train exp/tri5a exp/tri5a_ali || exit 1;
+# configure number of jobs running in parallel, you should adjust these numbers according to your machines
+# data preparation
+if [ $stage -le -2 ]; then
+  utils/utt2spk_to_spk2utt.pl data/train/utt2spk > data/train/spk2utt
+  rm -rf data/lang_train
+  mkdir -p data/tmp
+  utils/prepare_lang.sh data/local/dict "<UNK>"  data/tmp/lang_train data/lang_train
 
 
 fi
 
-# nnet3 tdnn models
-# commented out by default, since the chain model is usually faster and better
-#if [ $stage -eq 6 ]; then
+# Now make MFCC plus pitch features.
+# mfccdir should be some place with a largish disk where you
+# want to store MFCC features.
+mfccdir=mfcc
 
-  # echo "$0: train nnet3 model"
-  # local/nnet3/run_tdnn.sh
+# mfcc
+if [ $stage -le -1 ]; then
 
-#fi
+  echo "$0: making mfccs"
+  for x in train; do
+    steps/make_mfcc_pitch.sh --cmd "$train_cmd" --nj $num_jobs data/$x exp/make_mfcc/$x $mfccdir || exit 1;
+    steps/compute_cmvn_stats.sh data/$x exp/make_mfcc/$x $mfccdir || exit 1;
+    utils/fix_data_dir.sh data/$x || exit 1;
+  done
+
+fi
+
+# mono
+if [ $stage -le 0 ]; then
+
+  echo "$0: train mono model"
+  # Make some small data subsets for early system-build stages.
+  echo "$0: make training subsets"
+  utils/subset_data_dir.sh --shortest data/train 3000 data/train_mono
+
+  # train mono
+  steps/train_mono.sh --boost-silence 1.25 --cmd "$train_cmd" --nj $num_jobs \
+    data/train_mono data/lang exp/mono || exit 1;
+
+  # Get alignments from monophone system.
+  steps/align_si.sh --boost-silence 1.25 --cmd "$train_cmd" --nj $num_jobs \
+    data/train data/lang exp/mono exp/mono_ali || exit 1;
+
+
+fi
+
+# tri1
+if [ $stage -le 1 ]; then
+
+  echo "$0: train tri1 model"
+  # train tri1 [first triphone pass]
+  steps/train_deltas.sh --boost-silence 1.25 --cmd "$train_cmd" \
+   2500 20000 data/train data/lang exp/mono_ali exp/tri1 || exit 1;
+
+  # align tri1
+  steps/align_si.sh --cmd "$train_cmd" --nj $num_jobs \
+    data/train data/lang exp/tri1 exp/tri1_ali || exit 1;
+
+
+fi
+
+# tri2
+if [ $stage -le 2 ]; then
+
+  echo "$0: train tri2 model"
+  # train tri2 [delta+delta-deltas]
+  steps/train_deltas.sh --cmd "$train_cmd" \
+   2500 20000 data/train data/lang exp/tri1_ali exp/tri2 || exit 1;
+
+  # align tri2b
+  steps/align_si.sh --cmd "$train_cmd" --nj $num_jobs \
+    data/train data/lang exp/tri2 exp/tri2_ali || exit 1;
+
+
+fi
+
+# tri3a
+if [ $stage -le 3 ]; then
+
+  echo "$-: train tri3 model"
+  # Train tri3a, which is LDA+MLLT,
+  steps/train_lda_mllt.sh --cmd "$train_cmd" \
+   2500 20000 data/train data/lang exp/tri2_ali exp/tri3a || exit 1;
+
+
+fi
+
+# tri4
+if [ $stage -le 4 ]; then
+
+  echo "$0: train tri4 model"
+  # From now, we start building a more serious system (with SAT), and we'll
+  # do the alignment with fMLLR.
+  steps/align_fmllr.sh --cmd "$train_cmd" --nj $num_jobs \
+    data/train data/lang exp/tri3a exp/tri3a_ali || exit 1;
+
+  steps/train_sat.sh --cmd "$train_cmd" \
+    2500 20000 data/train data/lang exp/tri3a_ali exp/tri4a || exit 1;
+
+  # align tri4a
+  steps/align_fmllr.sh  --cmd "$train_cmd" --nj $num_jobs \
+    data/train data/lang exp/tri4a exp/tri4a_ali
+
+
+fi
+
+# tri5
+if [ $stage -le 5 ]; then
+
+  echo "$0: train tri5 model"
+  # Building a larger SAT system.
+  steps/train_sat.sh --cmd "$train_cmd" \
+    3500 100000 data/train data/lang exp/tri4a_ali exp/tri5a || exit 1;
+
+  # align tri5a
+  steps/align_fmllr.sh --cmd "$train_cmd" --nj $num_jobs \
+    data/train data/lang exp/tri5a exp/tri5a_ali || exit 1;
+
+
+fi
+
+exit 0
 
 # chain model
-if [ $stage -eq 7 ]; then
+if [ $stage -le 7 ]; then
 
   # The iVector-extraction and feature-dumping parts coulb be skipped by setting "--train_stage 7"
   echo "$0: train chain model"
@@ -48,7 +149,7 @@ if [ $stage -eq 7 ]; then
 fi
 
 # getting results (see RESULTS file)
-if [ $stage -eq 10 ]; then
+if [ $stage -le 10 ]; then
 
   echo "$0: extract the results"
   for x in exp/*/decode_test; do [ -d $x ] && grep WER $x/cer_* | utils/best_wer.sh; done 2>/dev/null
